c44bdcd1710d1272e083b0541289dbecc72c16ca
Benjamin Havens <benjaminhavens@byu.net>
2022-12-02 13:20:06 -0800

    Moved all to Arm Control and wrote MotorManager

---
+++ catkin_ws/src/ArmControl/MotorManager.py
@@ -0,0 +1,66 @@
+"""
+ROS node that connects to the hardware
+
+Responsibilities:
+- receives commands on the /motor_commands topic and accordingly commands the motors.
+- periodically reads the positions of the motors and publishes them
+"""
+
+import rospy
+from control_msgs import JointJog
+from sensor_msgs import JointState
+import parameters as p
+import Ax12 as Ax12
+
+
+class MotorManager:
+    def __init__(self):
+        # Connect to motors
+        Ax12.DEVICENAME = p.DEVICE_NAME
+        Ax12.BAUDRATE = p.BAUDRATE
+        Ax12.connect()
+
+        # Set up motors
+        m1 = Ax12(1)
+        m2 = Ax12(2)
+        m3 = Ax12(3)
+        self.motors = [m1, m2, m3]
+        for m in self.motors:
+            m.set_moving_speed(p.SPEED)
+
+        # SUBSCRIBERS
+        self.last_cmds = None
+        self.sub_cmds = rospy.Subscriber("/motor_commands", JointJog, self.save_cmds)
+
+        # PUBLISHERS
+        self.pub_pos = rospy.Publisher("/joint_state", JointState)
+
+    def save_cmds(self, motor_cmd_msg):
+        self.last_cmds = [p.angles_to_cmds(d) for d in motor_cmd_msg.displacements]
+
+    def send_cmds(self):
+        if self.last_cmds is None:
+            return
+        for m, cmd in zip(self.motors, self.last_cmds):
+            m.set_goal_position(cmd)
+
+    def update_positions(self):
+        msg = JointState()
+        msg.positions = [p.cmd_to_angle(m.get_present_position()) for m in self.motors]
+        self.pub_pos(msg)
+
+    def disconnect(self):
+        [m.set_torque_enable(0) for m in self.motors]
+        Ax12.disconnect()
+
+
+if __name__ == "__main__":
+    rospy.init_node("Motor Manager")
+    rate = rospy.Rate(p.WORKING_FREQ)
+    manager = MotorManager()
+
+    while not rospy.is_shutdown():
+        manager.update_positions()
+        manager.send_cmds()
+
+        rate.sleep()
--- catkin_ws/src/ArmControl/parameters.py
+++ catkin_ws/src/ArmControl/parameters.py
@@ -10,6 +10,7 @@ Isaac Shaw, Joey LeCheminant, and Benjamin Havens
 import roboticstoolbox as rtb
 import matplotlib.pyplot as plt
 from enum import Enum, auto
+from numpy import pi
 
 
 #########
@@ -20,23 +21,51 @@ class ArmControlMode(Enum):
     IK_BASE_FRAME = auto()  # IK in the base frame
 
 
-##############
-# PARAMETERS #
-##############
-WORKING_FREQ = 10
-PI = 3.14159265358979323846
+#################
+# MISCELLANEOUS #
+#################
+WORKING_FREQ = 10  # hz
+CENTER_ANGLE = 512  # Corresponds to 0 position
+HALF_RANGE = 308  # Corresponds to 90deg rotation
+QLIM_BOTTOM = CENTER_ANGLE - HALF_RANGE
+QLIM_TOP = CENTER_ANGLE + HALF_RANGE
+
+
+def angle_to_cmd(angle, radians=False):
+    if radians:
+        angle *= 180 / pi
+    return int(CENTER_ANGLE + (HALF_RANGE / 90) * angle)
+
+
+def cmd_to_angle(cmd, radians=False):
+    angle = float((cmd - CENTER_ANGLE) * 90 / HALF_RANGE)
+    if radians:
+        angle *= pi / 180
+    return angle
+
+
+####################
+# MOTOR CONNECTION #
+####################
+DEVICE_NAME = "/dev/ttyUSB0"
+BAUDRATE = int(1e6)
+SPEED = 100
+
+
+############
+# DH MODEL #
+############
 LINK_LENGTH = 0.15  # 15cm
 CLAW_LENGTH = 0.094
 
 PLANAR_DH = [
     rtb.RevoluteDH(a=LINK_LENGTH),
-    rtb.RevoluteDH(a=LINK_LENGTH, alpha=PI / 2),
-    rtb.RevoluteDH(a=CLAW_LENGTH, alpha=-PI / 2),
+    rtb.RevoluteDH(a=LINK_LENGTH, alpha=pi / 2),
+    rtb.RevoluteDH(a=CLAW_LENGTH, alpha=-pi / 2),
 ]
-
 N = len(PLANAR_DH)
 
-
+# Constructor that builds a DHRobot with our parameters
 class PlanarArm(rtb.DHRobot):
     def __init__(self):
         super().__init__(PLANAR_DH, name="Mini Rover Arm")
@@ -80,23 +109,21 @@ if __name__ == "__main__":
     arm = PlanarArm()
     print("\n", arm, "\n", sep="")
 
-    # Show the arm in all 0 configuration
-    q0 = [0] * N
+    # Show the arm in all negative configuration
+    q0 = [-pi / 4] * N
     T0 = arm.fkine(q0)
     print(f"When q=[0, 0, 0], the end effector is at\n{T0}\n")
     fig_1 = plt.figure(1)
     arm.plot(q0, fig=fig_1)
 
-    # Show the arm in another configuration
-    q1 = [PI / 4] * N
+    # Show the arm in all positive configuration
+    q1 = [pi / 4] * N
     T1 = arm.fkine(q1)
     print(f"When q=[pi/4, pi/4, pi/4], the end effector is at\n{T1}\n")
     fig_2 = plt.figure(2)
     arm.plot(q1, fig=fig_2)
 
-    # Show the arm moving from 0 to the second configuration
+    # Show the arm moving from the first to the second configuration
     trajectory = arm.jtraj(T0, T1, t=200)  # use t steps
     fig_3 = plt.figure(3)
     arm.plot(trajectory.q, fig=fig_3, block=True)
-
-# %%
